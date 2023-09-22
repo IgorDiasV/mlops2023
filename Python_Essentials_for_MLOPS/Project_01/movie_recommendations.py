f17a714273861e10fe6783d387480d2dd76b15a7
Igor Dias <igorverissimo02@hotmail.com>
2023-09-22 09:40:52 -0300

    added conditional to avoid code execution when importing a function in another file

--- Python_Essentials_for_MLOPS/Project_01/movie_recommendations.py
+++ Python_Essentials_for_MLOPS/Project_01/movie_recommendations.py
@@ -79,62 +79,63 @@ def on_type_recommendation_list(data):
             display(find_similar_movies(movie_id))
 
 
-movies = pd.read_csv("ml-25m/movies.csv")
-movies["clean_title"] = movies["title"].apply(clean_title)
+if __name__ == "__main__":
+    movies = pd.read_csv("ml-25m/movies.csv")
+    movies["clean_title"] = movies["title"].apply(clean_title)
 
-vectorizer = TfidfVectorizer(ngram_range=(1, 2))
+    vectorizer = TfidfVectorizer(ngram_range=(1, 2))
 
-tfidf = vectorizer.fit_transform(movies["clean_title"])
+    tfidf = vectorizer.fit_transform(movies["clean_title"])
 
-movie_input = widgets.Text(
-    value='Toy Story',
-    description='Movie Title:',
-    disabled=False
-)
-movie_list = widgets.Output()
+    movie_input = widgets.Text(
+        value='Toy Story',
+        description='Movie Title:',
+        disabled=False
+    )
+    movie_list = widgets.Output()
 
-movie_input.observe(on_type_movie_input, names='value')
+    movie_input.observe(on_type_movie_input, names='value')
 
-display(movie_input, movie_list)
+    display(movie_input, movie_list)
 
-MOVIE_ID = 89745
+    MOVIE_ID = 89745
 
-# def find_similar_movies(movie_id):
-movie = movies[movies["movieId"] == MOVIE_ID]
+    # def find_similar_movies(movie_id):
+    movie = movies[movies["movieId"] == MOVIE_ID]
 
-ratings = pd.read_csv("ml-25m/ratings.csv")
+    ratings = pd.read_csv("ml-25m/ratings.csv")
 
-similar_users = ratings[(ratings["movieId"] == MOVIE_ID) &
-                        (ratings["rating"] > 4)]["userId"].unique()
+    similar_users = ratings[(ratings["movieId"] == MOVIE_ID) &
+                            (ratings["rating"] > 4)]["userId"].unique()
 
-similar_user_recs = ratings[(ratings["userId"].isin(similar_users)) &
-                            (ratings["rating"] > 4)]["movieId"]
+    similar_user_recs = ratings[(ratings["userId"].isin(similar_users)) &
+                                (ratings["rating"] > 4)]["movieId"]
 
-similar_user_recs = similar_user_recs.value_counts() / len(similar_users)
+    similar_user_recs = similar_user_recs.value_counts() / len(similar_users)
 
-similar_user_recs = similar_user_recs[similar_user_recs > .10]
+    similar_user_recs = similar_user_recs[similar_user_recs > .10]
 
-all_users = ratings[(ratings["movieId"].isin(similar_user_recs.index)) &
-                    (ratings["rating"] > 4)]
+    all_users = ratings[(ratings["movieId"].isin(similar_user_recs.index)) &
+                        (ratings["rating"] > 4)]
 
-all_user_recs = user_recs(all_users)
+    all_user_recs = user_recs(all_users)
 
-rec_percentages = pd.concat([similar_user_recs, all_user_recs], axis=1)
-rec_percentages.columns = ["similar", "all"]
+    rec_percentages = pd.concat([similar_user_recs, all_user_recs], axis=1)
+    rec_percentages.columns = ["similar", "all"]
 
-rec_percentages["score"] = rec_percentages["similar"] / rec_percentages["all"]
+    rec_percentages["score"] = rec_percentages["similar"] / rec_percentages["all"]
 
-rec_percentages = rec_percentages.sort_values("score", ascending=False)
+    rec_percentages = rec_percentages.sort_values("score", ascending=False)
 
-rec_percentages.head(10).merge(movies, left_index=True, right_on="movieId")
+    rec_percentages.head(10).merge(movies, left_index=True, right_on="movieId")
 
-movie_name_input = widgets.Text(
-    value='Toy Story',
-    description='Movie Title:',
-    disabled=False
-)
-recommendation_list = widgets.Output()
+    movie_name_input = widgets.Text(
+        value='Toy Story',
+        description='Movie Title:',
+        disabled=False
+    )
+    recommendation_list = widgets.Output()
 
-movie_name_input.observe(on_type_recommendation_list, names='value')
+    movie_name_input.observe(on_type_recommendation_list, names='value')
 
-display(movie_name_input, recommendation_list)
+    display(movie_name_input, recommendation_list)
