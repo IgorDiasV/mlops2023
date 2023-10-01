24410ec893d2837387f18ded5268ca8f14fe10d2
Igor Dias <igorverissimo02@hotmail.com>
2023-10-01 17:24:45 -0300

    correcting import order

--- Python_Essentials_for_MLOPS/Project_01/movie_recommendations.py
+++ Python_Essentials_for_MLOPS/Project_01/movie_recommendations.py
@@ -1,11 +1,12 @@
 import re
+import logging
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 import ipywidgets as widgets
 from IPython.display import display
-import logging
+
 
 logging.basicConfig(level=logging.INFO)
 
