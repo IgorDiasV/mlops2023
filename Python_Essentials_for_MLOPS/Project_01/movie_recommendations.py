79c4352bc8f7650b3e10416e02be4729fd5d6ff0
Igor Dias <54191949+IgorDiasV@users.noreply.github.com>
2023-10-09 18:46:57 +0000

    refactor code

--- Python_Essentials_for_MLOPS/Project_01/movie_recommendations.py
+++ Python_Essentials_for_MLOPS/Project_01/movie_recommendations.py
@@ -9,6 +9,7 @@ import ipywidgets as widgets
 from IPython.display import display
 
 DATA_DIR = "Python_Essentials_for_MLOPS/Project_01/ml-25m/"
+MOVIE_ID = 89745
 
 logging.basicConfig(level=logging.INFO)
 
@@ -71,8 +72,7 @@ def on_type_movie_input(data):
             display(search_similar_movies(title))
             logging.info("Finished the search for similar films")
 
-
-def find_similar_movies(movie_id: int) -> pd.DataFrame:
+def find_recommendations(movie_id: int) -> pd.DataFrame:
     """search for recommendation movies"""
 
     similar_users = ratings[(ratings["movieId"] == movie_id) &
@@ -94,14 +94,13 @@ def find_similar_movies(movie_id: int) -> pd.DataFrame:
     rec_percentages.columns = ["similar", "all"]
     rec_percentages["score"] = rec_percentages["similar"] / rec_percentages["all"]
     rec_percentages = rec_percentages.sort_values("score", ascending=False)
- 
+
     top_recpercentages = rec_percentages.head(10)
     similar_movies = top_recpercentages.merge(movies,
                                               left_index=True,
                                               right_on="movieId")
     similar_movies = similar_movies[["score", "title", "genres"]]
     return similar_movies
-
 
 def on_type_recommendation_list(data):
     """displays movie recommendation"""
@@ -113,55 +112,20 @@ def on_type_recommendation_list(data):
             logging.info("starting the search for similar films")
             results = search_similar_movies(title)
             movie_id = results.iloc[0]["movieId"]
-            display(find_similar_movies(movie_id))
+            display(find_recommendations(movie_id))
             logging.info("Finished the search for recommended films")
 
 
 if __name__ == "__main__":
 
     movies, ratings = load_files()
-
     movies["clean_title"] = movies["title"].apply(clean_title)
 
     vectorizer = TfidfVectorizer(ngram_range=(1, 2))
-
     tfidf = vectorizer.fit_transform(movies["clean_title"])
 
     movie_input, movie_list = create_widget(on_type_movie_input)
-
     display(movie_input, movie_list)
 
-    MOVIE_ID = 89745
-
-    movie = movies[movies["movieId"] == MOVIE_ID]
-
-    similar_users = ratings[(ratings["movieId"] == MOVIE_ID) &
-                            (ratings["rating"] > 4)]["userId"].unique()
-
-    similar_user_recs = ratings[(ratings["userId"].isin(similar_users)) &
-                                (ratings["rating"] > 4)]["movieId"]
-
-    try:
-        similar_user_recs = similar_user_recs.value_counts() / len(similar_users)
-    except ZeroDivisionError:
-        logging.error("no similar users found")
-
-    similar_user_recs = similar_user_recs[similar_user_recs > .10]
-
-    all_users = ratings[(ratings["movieId"].isin(similar_user_recs.index)) &
-                        (ratings["rating"] > 4)]
-
-    all_user_recs = calculate_user_recs(all_users)
-
-    rec_percentages = pd.concat([similar_user_recs, all_user_recs], axis=1)
-    rec_percentages.columns = ["similar", "all"]
-
-    rec_percentages["score"] = rec_percentages["similar"] / rec_percentages["all"]
-
-    rec_percentages = rec_percentages.sort_values("score", ascending=False)
-
-    rec_percentages.head(10).merge(movies, left_index=True, right_on="movieId")
-
     movie_name_input, recommendation_list = create_widget(on_type_recommendation_list)
-    
     display(movie_name_input, recommendation_list)
