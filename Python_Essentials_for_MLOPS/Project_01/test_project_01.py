4f69937e7d57d37ec4f458b0d604d987a5d64e6b
Igor Dias <igorverissimo02@hotmail.com>
2023-10-09 14:07:28 -0300

    add test

--- Python_Essentials_for_MLOPS/Project_01/test_project_01.py
+++ Python_Essentials_for_MLOPS/Project_01/test_project_01.py
@@ -1,6 +1,7 @@
 
-from movie_recommendations import clean_title, load_files
+from movie_recommendations import clean_title, load_files, create_widget
 import pandas as pd
+import ipywidgets as widgets
 
 
 def test_clean_title():
@@ -16,3 +17,13 @@ def test_load_files():
 
     assert isinstance(movies, pd.DataFrame)
     assert isinstance(ratings, pd.DataFrame)
+
+
+def test_create_widget():
+    def teste():
+        return True
+
+    widget_input, widget_output = create_widget(teste)
+
+    isinstance(widget_input, widgets.Text)
+    isinstance(widget_output, widgets.Output)
