b973e5f9d0aa15f9efe9e0356bf324fd4029baa4
Igor Dias <igorverissimo02@hotmail.com>
2023-10-09 22:44:02 -0300

    add test

--- Python_Essentials_for_MLOPS/Project_03/test_customer_segmentation.py
+++ Python_Essentials_for_MLOPS/Project_03/test_customer_segmentation.py
@@ -1,8 +1,38 @@
 import pandas as pd
-from credit_card_customer_segmentation import load_file
+import numpy as np
+from credit_card_customer_segmentation import load_file, preprocess_customer_data
+from credit_card_customer_segmentation import find_optimal_clusters, train_kmeans_model
 
 
 def test_load_file():
     customers = load_file()
 
     assert isinstance(customers, pd.DataFrame)
+
+
+def test_preprocess_customer_data():
+    df = load_file()
+
+    preprocessed_data, preprocess_data_scaled = preprocess_customer_data(df)
+
+    first_value = -0.1654055800960332
+
+    assert preprocess_data_scaled[0][0] == first_value
+    assert isinstance(preprocessed_data, pd.DataFrame)
+
+
+def test_optimal_clusters():
+    df = load_file()
+    preprocessed_data, _ = preprocess_customer_data(df)
+    optimal_clusters = find_optimal_clusters(preprocessed_data)
+
+    assert isinstance(optimal_clusters, list)
+
+
+def test_train_kmeans_model():
+    df = load_file()
+    _, preprocess_data_scaled = preprocess_customer_data(df)
+    train_model = train_kmeans_model(preprocess_data_scaled)
+
+    assert isinstance(train_model, np.ndarray)
+
