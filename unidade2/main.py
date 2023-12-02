3d5bc3922363bb3b176c34c95f457b8dcd59e031
matheusslr <matheusslr@hotmail.com>
2023-12-02 11:05:20 -0300

    feat: adding eda to pipeline

--- unidade2/main.py
+++ unidade2/main.py
@@ -1,5 +1,6 @@
 from pipeline import Pipeline
 from fetch_data import fetch_data
+from eda import eda
 from preprocessing import preprocessing
 from data_check import data_check
 from data_segregation import data_segregation
@@ -12,6 +13,12 @@ pipeline = Pipeline()
 def run_fetch_data():
     print("running fetch data")
     fetch_data()
+
+
+@pipeline.task(depends_on=run_fetch_data)
+def run_eda():
+    print("running eda")
+    eda()
 
 
 @pipeline.task(depends_on=run_fetch_data)
